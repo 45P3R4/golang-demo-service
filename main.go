49a4e0b20f7eaf41fc200fb43f1cb813870dd0d4
45P3R4 <langleybaka@gmail.com>
2025-07-07 15:42:58 +0300

    error handling

--- main.go
+++ main.go
@@ -3,28 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-)
+	"os"
 
-// var cache map[string]Order
+	"github.com/google/uuid"
+)
 
 func getOrderById(w http.ResponseWriter, req *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "*") //Temp solution, unknown context
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow", "GET")
 
-	id := req.PathValue("id")
-
+	//Error handling
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Fprintf(w, "Order not found")
+			file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err != nil {
+				log.Fatal("Failed to open log file:", err)
+			}
+			log.SetOutput(file)
+			log.Println("ERROR: ", r)
 		}
 	}()
 
-	data, err := DbGetRowById(id)
+	//id string to UUID
+	id, err := uuid.Parse(req.PathValue("id"))
 	if err != nil {
-		fmt.Fprintf(w, "Failed to get data ftom database: %v", err)
+		fmt.Println("Failed to parse UUID: ", err)
+	}
+
+	var data Order
+
+	value, isMapContainsKey := cache[id]
+
+	//Get data
+	if isMapContainsKey {
+		//From cache
+		data = value
+	} else {
+		//From db
+		data, err = DbGetRowById(id.String())
+		if err != nil {
+			fmt.Fprintf(w, "Failed to get data ftom database: %v", err)
+		}
+		cache[id] = data
 	}
 
 	dataJson, err := json.Marshal(data)
@@ -36,6 +60,20 @@ func getOrderById(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
+	//Error handling
+	defer func() {
+		if r := recover(); r != nil {
+			file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err != nil {
+				log.Fatal("Failed to open log file:", err)
+			}
+			log.SetOutput(file)
+			log.Println("ERROR: ", r)
+		}
+	}()
+
+	fillCache()
 
 	go kafkaListen()
 
