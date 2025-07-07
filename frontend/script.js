ef19fd5166b1c343c47fb46e6842043837409b5c
45P3R4 <langleybaka@gmail.com>
2025-07-07 15:43:10 +0300

    frontend fix

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,12 @@ var button = document.getElementById("submit-uid-btn")
 
     console.log(input.value)
 
-       fetch('http://localhost:8081/order/' + input.value)
+       fetch('http://localhost:8081/order/' + input.value, {
+          mode: 'cors',
+          headers: {
+            'Origin': 'http://localhost:8081'
+          }
+       })
     .then(result => result.json())
     .then((orderData) => {
       console.log(orderData)
@@ -14,45 +19,46 @@ var button = document.getElementById("submit-uid-btn")
           htmlOrderString = `
       <div class="order-section">
         <h2>Order Information</h2>
-        <p><span class="label">Order ID:</span> ${orderData.order_uid}</p>
-        <p><span class="label">Track Number:</span> ${orderData.track_number}</p>
-        <p><span class="label">Date Created:</span> ${orderData.date_created}</p>
+        <p><div>Order ID: ${orderData.order_uid}</div></p>
+        <p><div>Track Number: ${orderData.track_number}</div></p>
+        <p><div>Date Created: ${orderData.date_created}</div></p>
       </div>
 
       <div class="order-section">
         <h2>Delivery Details</h2>
-        <p><span class="label">Name:</span> ${orderData.delivery.name}</p>
-        <p><span class="label">Address:</span> ${orderData.delivery.address}, ${orderData.delivery.city}</p>
-        <p><span class="label">Phone:</span> ${orderData.delivery.phone}</p>
-        <p><span class="label">Email:</span> ${orderData.delivery.email}</p>
+        <p><div>Name: ${orderData.delivery.name}</div></p>
+        <p><div>Address: ${orderData.delivery.address}, ${orderData.delivery.city}</div></p>
+        <p><div>Phone: ${orderData.delivery.phone}</div></p>
+        <p><div>Email: ${orderData.delivery.email}</div></p>
       </div>
 
       <div class="order-section">
         <h2>Payment Information</h2>
-        <p><span class="label">Amount:</span> ${orderData.payment.amount} ${orderData.payment.currency}</p>
-        <p><span class="label">Payment Date:</span> ${orderData.payment.payment_dt}</p>
-        <p><span class="label">Provider:</span> ${orderData.payment.provider}</p>
+        <p><div>Amount: ${orderData.payment.amount} ${orderData.payment.currency}</div></p>
+        <p><div>Payment Date: ${orderData.payment.payment_dt}</div></p>
+        <p><div>Provider: ${orderData.payment.provider}</div></p>
       </div>
     `;
+
+    htmlItemString = ""
 
       orderData.items.forEach(item => {
-        htmlItemString = `
-      <div class="order-section">
-      <h2>Item Information</h2>
-        <p><span class="label">Product:</span> ${item.name} (${item.brand})</p>
-        <p><span class="label">Price:</span> ${item.price} (${item.sale}% off)</p>
-        <p><span class="label">Total:</span> ${item.total_price}</p>
+        htmlItemString += `
+      <div class="item-card">
+        <h2>${item.name} (${item.brand})</h2>
+        <p><div>Price: ${item.price} (${item.sale}% off)</div> </p>
+        <p><div>Total: ${item.total_price}</div></p>
       </div>
     `;
-
-    htmlOrderString += htmlItemString;
         
       });
       
     container.innerHTML = htmlOrderString;
+    item_container.innerHTML = htmlItemString;
 
     })
     .catch(err => console.error(err));
 
     var container = document.getElementById("order-container");
+    var item_container = document.getElementById("item-container");
     };
