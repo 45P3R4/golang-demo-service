ab000feeff953f3967702d4d1c0e4eb112268be7
45P3R4 <langleybaka@gmail.com>
2025-07-07 17:32:14 +0300

    cache improved

--- cache.go
+++ cache.go
@@ -4,11 +4,31 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/patrickmn/go-cache"
 )
 
 const cacheSize = 10
 
-var cache = map[uuid.UUID]Order{}
+var DbCache *cache.Cache
+
+func tryGetFromCache(id uuid.UUID) (Order, error) {
+
+	var data Order
+
+	var err error
+
+	order, found := DbCache.Get(id.String())
+	if found {
+		//From cache
+		data = order.(Order)
+	} else {
+		//From db
+		data, err = DbGetRowById(id.String())
+		DbCache.Set(id.String(), data, cache.DefaultExpiration)
+	}
+
+	return data, err
+}
 
 func fillCache() {
 	orders, err := DbGetLastRows(cacheSize)
@@ -16,6 +36,7 @@ func fillCache() {
 		fmt.Println("Failed to fill cache ", err)
 	}
 	for _, order := range orders {
-		cache[order.OrderUID] = order
+		// cache[order.OrderUID] = order
+		DbCache.Set(order.OrderUID.String(), order, 0)
 	}
 }
